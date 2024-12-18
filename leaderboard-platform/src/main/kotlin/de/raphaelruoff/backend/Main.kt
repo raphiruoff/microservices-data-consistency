e1bd8036b5ed59e6414a8da2f971d4dc940820c8
Raphael Ruoff <raphael.ruoff@gmx.de>
2024-12-18 10:23:49 +0100

    renaming

--- leaderboard-platform/src/main/kotlin/de/raphaelruoff/backend/Main.kt
+++ leaderboard-platform/src/main/kotlin/de/raphaelruoff/backend/Main.kt
@@ -1,4 +1,4 @@
-package de.raphaelruoff.microservice.microservic_data_consistency
+package de.raphaelruoff.backend.profilemanagment
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
